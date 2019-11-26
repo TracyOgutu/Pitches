bfb9a2835c3a976a31c2cf070365b50cf438f69c
Tracy Ogutu <tracyogutu@gmail.com>
2019-11-26 05:40:32 +0300

    add tests

---
+++ tests/test_downvote.py
@@ -0,0 +1,12 @@
+import unittest
+
+from app.model import Downvote
+
+class DownvoteModelTest(unittest.TestCase):
+
+    def setUp(self):
+        self.new_downvote=Downvote(downvote=1)
+
+    def test_save_downvotes(self):
+        self.new_downvote.save_downvotes()
+        self.assertTrue(len(Downvote.query.all())>0)
